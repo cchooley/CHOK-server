9848650439585c89f249106f7f1ce0f737c59971
Onder Gunacan <ondergunacan@Onders-MacBook-Air.local>
2018-06-26 14:26:23 -0600

    database-connection.js refactored

--- database-connection.js
+++ database-connection.js
@@ -1,7 +1,12 @@
-const environment = process.env.NODE_ENV || 'development'
+/* const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')
 const environmentConfig = config[environment]
 const knex = require('knex')
 const connection = knex(environmentConfig)
 
 module.exports = connection
+
+*/
+
+const CONFIG = require('./knexfile')[process.env.NODE_ENV || 'development'];
+module.exports = require('knex')(CONFIG)
