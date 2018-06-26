92c551bed71b0844a588f81408a4f3c534fd54ed
Onder Gunacan <ondergunacan@Onders-MacBook-Air.local>
2018-06-26 14:44:24 -0600

    Refactoring app.listen in app.js

--- app.js
+++ app.js
@@ -31,8 +31,6 @@ app.use(function(err, req, res, next) {
 });
 
 const port = process.env.PORT || 5000
-app.listen(port, () => {
-  console.log(`Listening on ${port}`)
-})
+app.listen(process.env.PORT || port)
 
 module.exports = app;
